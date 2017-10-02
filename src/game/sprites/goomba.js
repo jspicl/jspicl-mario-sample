6a7fe0bb18281bf4d9c7480d556c0589411b4558
agronkabashi <agron@cerberussolutions.se>
2017-10-02 12:54:33 +0200

    Adjusted animation speed

--- src/game/sprites/goomba.js
+++ src/game/sprites/goomba.js
@@ -2,5 +2,5 @@ import { createKeyframeAnimation } from "../factories";
 
 export const goomba = {
   default: createKeyframeAnimation(5, 1, 1, 1, 1),
-  run: createKeyframeAnimation(5, 2, 0.5, 1, 1)
+  run: createKeyframeAnimation(5, 2, 0.3, 1, 1)
 };
--- src/game/sprites/koopa.js
+++ src/game/sprites/koopa.js
@@ -2,5 +2,5 @@ import { createKeyframeAnimation } from "../factories";
 
 export const koopa = {
   default: createKeyframeAnimation(7, 1, 1, 1, 1),
-  run: createKeyframeAnimation(7, 2, 0.5, 1, 1)
+  run: createKeyframeAnimation(7, 2, 0.3, 1, 1)
 };
