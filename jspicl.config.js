df0bb7f89cc14b15f8c52dc9127ab70e7b8d3693
Agron Kabashi <dadgron@gmail.com>
2026-02-14 01:46:31 +0100

    Upgrade to jspicl 4 alpha

---
+++ jspicl.config.js
@@ -0,0 +1,12 @@
+export default {
+  jsOutput: "build/jsOutput.js",
+  spritesheetImagePath: "./spritesheet.png",
+  includeBanner: true,
+  showStats: true,
+  jspiclOptions: {
+    prettify: true,
+  },
+  picoOptions: {
+    cartDataId: "mygame",
+  },
+};
--- src/index.js
+++ src/index.js
@@ -1,20 +1,15 @@
+/// <reference types="@jspicl/core/pic8-api.d.ts" />
 import { init, draw, update } from "./game";
 
 /* eslint-disable no-underscore-dangle */
-function _init () {
+function _init() {
   init();
 }
 
-function _update60 () {
+function _update60() {
   update(1 / 60);
 }
 
-function _draw () {
+function _draw() {
   draw();
 }
-
-// <!-- DEBUG
-_init();
-_update60();
-_draw();
-// -->
